5cb212071ba0f25c64e8519e1e69bba9520188a3
bot5294 <5294aqib@gmail.com>
2022-07-30 16:20:29 +0530

    Project completed

---
+++ build/precache-manifest.37c27f774576c1272c4ffe1832f34b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a56db35deacd9414bcffdde16447c4",
+    "url": "/todo_react/index.html"
+  },
+  {
+    "revision": "2497dd3370715ee6077c",
+    "url": "/todo_react/static/css/main.139fed39.chunk.css"
+  },
+  {
+    "revision": "3bbb02b220a767f4da0d",
+    "url": "/todo_react/static/js/2.e4aa5250.chunk.js"
+  },
+  {
+    "revision": "49c45e15b22906ea61e3b3085d227f58",
+    "url": "/todo_react/static/js/2.e4aa5250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2497dd3370715ee6077c",
+    "url": "/todo_react/static/js/main.108f7f5d.chunk.js"
+  },
+  {
+    "revision": "12980a9097b12630245f",
+    "url": "/todo_react/static/js/runtime-main.b6a2f3ad.js"
+  }
+]);

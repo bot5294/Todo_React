54db94eb5c76307fef579ca0b889b9948d35b6a3
bot5294 <5294aqib@gmail.com>
2022-07-30 18:06:26 +0530

    Updates

---
+++ precache-manifest.3c0478ed02fba0ce5fba95350f34cddd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "248d8427a369abe85de4",
+    "url": "/todo_react/static/css/main.0bc76743.chunk.css"
+  },
+  {
+    "revision": "248d8427a369abe85de4",
+    "url": "/todo_react/static/js/main.248d8427.chunk.js"
+  },
+  {
+    "revision": "0802e8408ad63ba56afc",
+    "url": "/todo_react/static/js/1.0802e840.chunk.js"
+  },
+  {
+    "revision": "657a331f25867b90a62f",
+    "url": "/todo_react/static/js/runtime~main.657a331f.js"
+  },
+  {
+    "revision": "31de2091d86d54fd40a769ba5c83e05a",
+    "url": "/todo_react/index.html"
+  }
+];
